63f173bb90e182335cd97843cf9cc6259400c221
Joshua Needham <joshuabneedham@gmail.com>
2020-08-07 19:03:11 -0500

    Updated to include some information about the repo.

--- front-end/src/components/pages/Home.jsx
+++ front-end/src/components/pages/Home.jsx
@@ -6,6 +6,22 @@ const Home = (props) => {
 			{console.log(props.loggedInStatus)}
 			<h1>Home Page</h1>
 			Session Info: {props.loggedInStatus}
+			<p className='lead'>
+				This was built watching Jordan Hudgens YouTube series on React + Rails
+				API Authentication. Jordan's tutorials have help me greatly in my
+				learning of Ruby, Rails, and now React.
+			</p>
+			<iframe
+				width='560'
+				height='315'
+				src='https://www.youtube.com/embed/videoseries?list=PLgYiyoyNPrv_yNp5Pzsx0A3gQ8-tfg66j'
+				frameborder='0'
+				allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+				allowfullscreen></iframe>
+			<p className='lead'>
+				Feel free to use this application to help you get started building Rails
+				backend with React front ends.{' '}
+			</p>
 		</div>
 	);
 };
